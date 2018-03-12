518278525462a8d4c773bbd21eee945f766a6a49
2903710916 <33981218+2903710916@users.noreply.github.com>
2018-03-12 20:42:03 +0800

    Update axjs_upgrade.sh
    u

--- axjs_upgrade.sh
+++ axjs_upgrade.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 killall node
-rm -rf ./package-lock.json ./yarn.lock ./yarn-error.log
+rm -rf ./package-lock.json ./npm-debug.log ./yarn.lock ./yarn-error.log
 rm -rf ./node_modules
 yarn install
 
