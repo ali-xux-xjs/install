b3c0c87df1b470d9a649b2acb53b04cf5ca27a17
2903710916 <33981218+2903710916@users.noreply.github.com>
2018-03-12 20:38:46 +0800

    Update axjs_upgrade.sh
    改进命令实现避免npm run命令执行依赖冲突同时也是简化使用。

--- axjs_upgrade.sh
+++ axjs_upgrade.sh
@@ -4,3 +4,10 @@ killall node
 rm -rf ./package-lock.json ./yarn.lock ./yarn-error.log
 rm -rf ./node_modules
 yarn install
+
+echo "#!/bin/bash" >  ./cmd_make.sh
+echo "./node_modules/@ali/axjs/cmd_make.sh" >> ./cmd_make.sh
+echo "#!/bin/bash" >  ./cmd_debug.sh
+echo "./node_modules/@ali/axjs/cmd_debug.sh" >> ./cmd_debug.sh
+echo "#!/bin/bash" >  ./cmd_publish.sh
+echo "./node_modules/@ali/axjs/cmd_publish.sh" >> ./cmd_publish.sh
