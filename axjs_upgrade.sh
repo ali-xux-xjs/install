ba2cb3d6727680e590fbe2d7538b535861bbaaee
2903710916 <33981218+2903710916@users.noreply.github.com>
2018-03-12 21:10:50 +0800

    Update axjs_upgrade.sh
    u

--- axjs_upgrade.sh
+++ axjs_upgrade.sh
@@ -15,7 +15,7 @@ echo "./node_modules/@ali/axjs/cmd_upgrade.sh" >> ./cmd_upgrade.sh
 echo "#!/bin/bash" >  ./cmd_make.sh
 echo "./node_modules/@ali/axjs/cmd_make.sh" >> ./cmd_make.sh
 echo "#!/bin/bash" >  ./cmd_debug.sh
-echo "./node_modules/@ali/axjs/cmd_debug.sh" >> ./cmd_debug.sh
+echo "./node_modules/@ali/axjs/cmd_debug_app.sh" >> ./cmd_debug.sh
 echo "#!/bin/bash" >  ./cmd_publish.sh
 echo "./node_modules/@ali/axjs/cmd_publish.sh" >> ./cmd_publish.sh
 chmod +x ./cmd_upgrade.sh ./cmd_make.sh ./cmd_debug.sh ./cmd_publish.sh
