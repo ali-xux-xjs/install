f1904d9f01286aac6ad81cd140064c4b648cdf72
2903710916 <33981218+2903710916@users.noreply.github.com>
2018-03-13 06:31:56 +0800

    Update axjs_upgrade.sh
    u

--- axjs_upgrade.sh
+++ axjs_upgrade.sh
@@ -20,5 +20,5 @@ echo "    killall node; exit 0;" >> ./cmd_debug.sh
 echo "fi" >> ./cmd_debug.sh
 echo "./node_modules/@ali/axjs/cmd_debug_app.sh \$1" >> ./cmd_debug.sh
 echo "#!/bin/bash" >  ./cmd_publish.sh
-echo "./node_modules/@ali/axjs/cmd_publish.sh" >> ./cmd_publish.sh
+echo "./node_modules/@ali/axjs/cmd_publish.sh \$1" >> ./cmd_publish.sh
 chmod +x ./cmd_upgrade.sh ./cmd_make.sh ./cmd_debug.sh ./cmd_publish.sh
